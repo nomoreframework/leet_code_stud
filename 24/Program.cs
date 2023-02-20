022614793f79f686cf128ba9c86fe86d5b33bfc4
nomoreframework <gleblukianenko@gmail.com>
2023-02-20 19:32:23 +0300

    added solution for problem 22

---
+++ 24/Program.cs
@@ -0,0 +1,20 @@
+﻿void swap(ref ListNode node){
+    if(node == null) return;
+    else if(node.next == null) return;
+    else{
+    ListNode buff  = new ListNode(node.val, node.next.next);
+    node = node.next;
+    node.next = buff;
+    var r = node.next.next;
+        swap(ref node.next.next);
+    }    
+}
+class ListNode {
+    public int val;
+    public ListNode next; 
+    public ListNode(int val = 0, ListNode next = null){
+          this.val = val;
+          this.next = next;
+    }
+}
+
