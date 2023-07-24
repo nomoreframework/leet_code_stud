029c091ee6b6aea0bc4fc6b1033c5702efbcd60c
nomoreframework <gleblukianenko@gmail.com>
2023-07-24 17:50:07 +0300

    added solutions for problems 209,242

---
+++ 209/Program.cs
@@ -0,0 +1,15 @@
+﻿var MinSubArrayLen = (int target, int[] nums) =>{
+        int arr_length = nums.Length;
+        int sum = 0;
+        int left_pointer = 0;
+        int length = int.MaxValue;
+        for(int i = 0; i < arr_length;i++){
+            sum += nums[i];
+            while(sum >= target){
+                length = Math.Min(length, i + 1 - left_pointer);
+                sum -= nums[left_pointer++];
+            }
+        }
+        return length != int.MaxValue ? length : 0;
+};
+Console.WriteLine(MinSubArrayLen(4,new int []{1,4,4}));
---
+++ 242/Program.cs
@@ -0,0 +1,19 @@
+﻿var IsAnagram = (string s, string t) =>
+{
+    bool result = true;
+    var HashT = new Dictionary<char, int>(s.Length);
+    var HashS = new Dictionary<char, int>(s.Length);
+    if (s.Length != t.Length) return false;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (HashT.TryAdd(s[i], 1)) { }
+        else HashT[s[i]]++;
+        if (HashS.TryAdd(t[i], 1)) { }
+        else HashS[t[i]]++;
+    }
+    foreach(var kvp in HashT){
+        if(HashS.TryAdd(kvp.Key,kvp.Value) || kvp.Value != HashS[kvp.Key]) return false;
+    }
+    return result;
+};
+Console.WriteLine(IsAnagram("aacc", "cacc"));
