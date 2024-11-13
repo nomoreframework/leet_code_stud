790a150c5b7b205ec0a547cd1ac1bcc02c5f0377
nomoreframework <gleblukianenko@gmail.com>
2024-11-14 00:04:36 +0300

    1431

---
+++ 1431/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+        int maxValue = 0;
+        List<bool> res = new ();
+        for(int i = 0; i < candies.Length;i++)
+                maxValue = candies[i] > maxValue ? candies[i] : maxValue;
+        for(int i = 0; i < candies.Length;i++)
+            res.Add(candies[i] + extraCandies>= maxValue);
+            return res;
+    }
+}
