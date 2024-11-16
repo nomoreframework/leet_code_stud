33c4e8273b2071f2c9af23d4728798281f7ad201
nomoreframework <gleblukianenko@gmail.com>
2024-11-16 16:50:18 +0300

    151

---
+++ 151/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+
+        int length = s.Length;
+        if (length == 1 || length == 0)
+            return s;
+
+        var substrList = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+        StringBuilder result = new(length);
+        for (int i = substrList.Length - 1; i > 0; i--)
+        {
+            result.Append(substrList[i]);
+            result.Append(' ');
+        }
+        result.Append(substrList[0]);
+        return result.ToString();
+    }
+}
