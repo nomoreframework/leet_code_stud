59733ad949920d1f1a6bc9525acd14cb66bf0e00
nomoreframework <gleblukianenko@gmail.com>
2024-11-20 00:28:52 +0300

    283

---
+++ 283/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int l = nums.Length - 1;
+        int j = 1;
+        for(int i = 0; i < l; i++){
+            if(nums[i] == 0){
+                while(nums[j] == 0 & j < l)
+                    j++;
+                nums[i] = nums[j];
+                nums[j] = 0;
+            }
+            else 
+                j++;
+        }
+    }
+}
